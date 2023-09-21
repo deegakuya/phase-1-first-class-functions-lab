57d9bec9522161b43783dddb0396ba2f006a3c6e
Diana Gakuya <deegakuya@gmail.com>
2023-09-21 20:16:39 +0300

    Assignment 2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+// returnFirstTwoDrivers function
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // returnLastTwoDrivers function
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers array
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier function
+  const createFareMultiplier = function(integer) {
+    return function(fare) {
+      return fare * integer;
+    };
+  };
+  
+  // fareDoubler function
+  const fareDoubler = createFareMuselectDifferentDriversltiplier(2);
+  
+  // fareTripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  //  function
+  const selectDifferentDrivers = function(drivers, driverSelector) {
+    return driverSelector(drivers);
+  };
+  
+  
+  
